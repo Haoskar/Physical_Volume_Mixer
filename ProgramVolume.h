8051b32b454766a3239c626c2a3677d24ede1294
Oskar <Oskar.hammargren@gmail.com>
2023-04-09 17:28:42 +0200

    it now uses a notification thingy and i have ditched the updater

--- ProgramVolume.h
+++ ProgramVolume.h
@@ -3,36 +3,271 @@
 #include <Audioclient.h>
 #include <strsafe.h>
 #include <iostream>
+#include <Audiopolicy.h>
+#include <Psapi.h>
+
+
+#define PRINT_ON_ERROR(hres)  \
+               if(FAILED(hres)) {printf("\n\nFAILURE: %X\n\n", hres);}
+
+//-----------------------------------------------------------
+// Client implementation of IAudioSessionEvents interface.
+// WASAPI calls these methods to notify the application when
+// a parameter or property of the audio session changes.
+//-----------------------------------------------------------
+class CAudioSessionEvents : public IAudioSessionEvents
+{
+private:
+	LONG _cRef;
+
+	~CAudioSessionEvents()
+	{
+	}
+
+public:
+	CAudioSessionEvents() :
+		_cRef(1)
+	{
+	}
+
+	// IUnknown methods -- AddRef, Release, and QueryInterface
+
+	ULONG STDMETHODCALLTYPE AddRef()
+	{
+		return InterlockedIncrement(&_cRef);
+	}
+
+	ULONG STDMETHODCALLTYPE Release()
+	{
+		ULONG ulRef = InterlockedDecrement(&_cRef);
+		if (0 == ulRef)
+		{
+			delete this;
+		}
+		return ulRef;
+	}
+
+	HRESULT STDMETHODCALLTYPE QueryInterface(
+		REFIID  riid,
+		VOID** ppvInterface)
+	{
+		if (IID_IUnknown == riid)
+		{
+			AddRef();
+			*ppvInterface = (IUnknown*)this;
+		}
+		else if (__uuidof(IAudioSessionEvents) == riid)
+		{
+			AddRef();
+			*ppvInterface = (IAudioSessionEvents*)this;
+		}
+		else
+		{
+			*ppvInterface = NULL;
+			return E_NOINTERFACE;
+		}
+		return S_OK;
+	}
+
+	// Notification methods for audio session events
+
+	HRESULT STDMETHODCALLTYPE OnDisplayNameChanged(
+		LPCWSTR NewDisplayName,
+		LPCGUID EventContext)
+	{
+		return S_OK;
+	}
+
+	HRESULT STDMETHODCALLTYPE OnIconPathChanged(
+		LPCWSTR NewIconPath,
+		LPCGUID EventContext)
+	{
+		return S_OK;
+	}
+
+	HRESULT STDMETHODCALLTYPE OnSimpleVolumeChanged(
+		float NewVolume,
+		BOOL NewMute,
+		LPCGUID EventContext)
+	{
+		/*if (NewMute)
+		{
+			printf("MUTE\n");
+		}
+		else
+		{
+			printf("Volume = %d percent\n",
+				(UINT32)(100 * NewVolume + 0.5));
+		}*/
+
+		return S_OK;
+	}
+
+	HRESULT STDMETHODCALLTYPE OnChannelVolumeChanged(
+		DWORD ChannelCount,
+		float NewChannelVolumeArray[],
+		DWORD ChangedChannel,
+		LPCGUID EventContext)
+	{
+		return S_OK;
+	}
+
+	HRESULT STDMETHODCALLTYPE OnGroupingParamChanged(
+		LPCGUID NewGroupingParam,
+		LPCGUID EventContext)
+	{
+		return S_OK;
+	}
+
+	HRESULT STDMETHODCALLTYPE OnStateChanged(
+		AudioSessionState NewState)
+	{
+		/*std::string pszState = "?????";
+
+		switch (NewState)
+		{
+		case AudioSessionStateActive:
+			pszState = "active";
+			break;
+		case AudioSessionStateInactive:
+			pszState = "inactive";
+			break;
+		case AudioSessionStateExpired:
+			pszState = "expired";
+			break;
+		}
+		printf("New session state = %s\n", pszState.c_str());*/
+
+		return S_OK;
+	}
+
+	HRESULT STDMETHODCALLTYPE OnSessionDisconnected(
+		AudioSessionDisconnectReason DisconnectReason)
+	{
+		/*std::string pszReason = "?????";
+
+		switch (DisconnectReason)
+		{
+		case DisconnectReasonDeviceRemoval:
+			pszReason = "device removed";
+			break;
+		case DisconnectReasonServerShutdown:
+			pszReason = "server shut down";
+			break;
+		case DisconnectReasonFormatChanged:
+			pszReason = "format changed";
+			break;
+		case DisconnectReasonSessionLogoff:
+			pszReason = "user logged off";
+			break;
+		case DisconnectReasonSessionDisconnected:
+			pszReason = "session disconnected";
+			break;
+		case DisconnectReasonExclusiveModeOverride:
+			pszReason = "exclusive-mode override";
+			break;
+		}
+		printf("Audio session disconnected (reason: %s)\n",
+			pszReason.c_str());*/
+
+		return S_OK;
+	}
+};
 
 class ProgramVolume
 {
 private:
-
-
-
-public:
-	int printable = TRUE;
-	ISimpleAudioVolume* pSimpleAudio;
-	wchar_t progName[2048] = L"";
-	DWORD progNameSize = 2048;
-	//LPWSTR displayName = NULL;
-
-	ProgramVolume(ISimpleAudioVolume* pSimpleAudio) {
-		if (pSimpleAudio == NULL)
-			throw ERROR_BAD_ARGUMENTS;
-		this->pSimpleAudio = pSimpleAudio;
-	}
-
-	ProgramVolume(const ProgramVolume& p) {
-		pSimpleAudio = p.pSimpleAudio;
-		progNameSize = p.progNameSize;
-		StringCbCopyW(progName, progNameSize, p.progName);
-
-
-	}
+	CAudioSessionEvents* audiosess = NULL;
 
 	~ProgramVolume() {
+		std::cout << "\n\nI AM KILLING MYSELF GODBYE: ";
+		std::wcout << display_name << "\n\n";
+		if (audiosess != NULL)
+			pAudioSessionControl->UnregisterAudioSessionNotification(audiosess);
+		if (pAudioSessionControl != NULL)
+			pAudioSessionControl->Release();
+		if (pSimpleAudio != NULL)
+			pSimpleAudio->Release();
+
 	}
+
+public:
+	IAudioSessionControl* pAudioSessionControl = NULL;
+	ISimpleAudioVolume* pSimpleAudio = NULL;
+
+	std::wstring display_name = L"";
+
+	ProgramVolume(IAudioSessionControl *pAudioSessionControl){
+		HRESULT hr = S_OK;
+
+		if (pAudioSessionControl == NULL)
+			throw ERROR_BAD_ARGUMENTS;
+
+		pAudioSessionControl->AddRef();
+		this->pAudioSessionControl = pAudioSessionControl;
+
+		hr = pAudioSessionControl->QueryInterface(__uuidof(ISimpleAudioVolume), (void**)&pSimpleAudio);
+		PRINT_ON_ERROR(hr);
+
+		
+		//get the name of the program
+		
+		wchar_t *tempDName;
+		pAudioSessionControl->GetDisplayName(&tempDName);
+		display_name.assign(tempDName);
+		std::cout << "My SessionControl name is: ";
+		std::wcout << display_name << L"\n";
+		CoTaskMemFree(tempDName);
+
+		DWORD processId = 0;
+		IAudioSessionControl2* pAudioSessionControl2 = NULL;
+		
+		if (display_name == L"") {
+			std::cout << "\nDisplay name is empty\n";
+			
+			hr = pAudioSessionControl->QueryInterface(__uuidof(IAudioSessionControl2), (void**)&pAudioSessionControl2);
+			PRINT_ON_ERROR(hr);
+			hr = pAudioSessionControl2->GetProcessId(&processId);
+			PRINT_ON_ERROR(hr);
+
+			const DWORD processPathLength = 1024;
+			wchar_t processPath[processPathLength] = L"";
+			if (GetProcessImageFileNameW(OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, FALSE, processId), processPath, processPathLength) != 0) {
+				wchar_t* wcs;
+				wcs = wcsrchr(processPath, L'.');
+				if (wcs != NULL) {
+					*wcs = L'\0';
+
+					wcs = wcsrchr(processPath, L'\\') + 1;
+					display_name.assign(wcs);
+
+				}
+			}
+		}
+
+		std::cout << "My final display name is: ";
+		std::wcout << display_name << L"\n\n";
+	}
+
+
+
+	bool release() {
+		if (pAudioSessionControl == NULL)
+			return false;
+
+		AudioSessionState state;
+		pAudioSessionControl->GetState(&state);
+
+		if (state == AudioSessionStateExpired) {
+			std::cout << "\n\nI AM RELEASING MYSELF GODBYE: ";
+			std::wcout << display_name << "\n\n";
+			delete this;
+			return true;
+		}
+
+		return false;
+	}
+
 	void mute() { pSimpleAudio->SetMute(TRUE, NULL); }
 	void unmute() { pSimpleAudio->SetMute(FALSE, NULL); }
 
@@ -61,8 +296,17 @@ public:
 	}
 
 	void print() {
-		std::wcout << "ProgName: " << progName;
+		std::wcout << "ProgName: " << display_name;
 		std::cout << "\nVolume: " << getVolume() << "\n";
 	}
 
+	void registerClient() {
+		audiosess = new CAudioSessionEvents();
+		if (audiosess == NULL)
+			return;
+		if (pAudioSessionControl == NULL)
+			return;
+		
+		pAudioSessionControl->RegisterAudioSessionNotification(audiosess);
+	}
 };
--- TestVolume_c++.cpp
+++ TestVolume_c++.cpp
@@ -12,7 +12,6 @@
 #include <Audiopolicy.h>
 #include <objbase.h>
 #include <strsafe.h>
-#include <Psapi.h>
 #include <unknwnbase.h>
 #include <vector>
 #include <thread>
@@ -33,8 +32,7 @@ std::mutex mtx;
               if ((punk) != NULL)  \
                 { (punk)->Release(); (punk) = NULL; }
 
-#define PRINT_ON_ERROR(hres)  \
-               if(FAILED(hres)) {printf("\n\nFAILURE: %X\n\n", hres);}
+
 
 const CLSID CLSID_MMDeviceEnumerator = __uuidof(MMDeviceEnumerator);
 const IID IID_IMMDeviceEnumerator = __uuidof(IMMDeviceEnumerator);
@@ -49,247 +47,145 @@ int check(enum sp_return result);
 
 
 
-std::vector<ProgramVolume> volumeVector;
+//std::vector<ProgramVolume> volumeVector;
 IMMDevice* pAudioEndPoint = NULL;
 IAudioClient* pAudioClient = NULL;
 IAudioSessionManager* pAudioSessionManager = NULL;
 IAudioSessionManager2* pAudioSessionManager2 = NULL;
 IAudioEndpointVolume* pAudioEndpointVolume = NULL;
 
-class AudioNotifications: public IAudioSessionNotification
-{
 
+
+
+class ProgVolumeList {
 private:
-
-        LONG             m_cRefAll;
-        ~AudioNotifications(){};
+	std::vector<size_t> newClients;
 
 public:
-    AudioNotifications(): m_cRefAll(1) {};
-
-    HRESULT STDMETHODCALLTYPE QueryInterface(REFIID riid, void **ppv)  
-    {    
-        if (IID_IUnknown == riid)
-        {
-            AddRef();
-            *ppv = (IUnknown*)this;
-        }
-        else if (__uuidof(IAudioSessionNotification) == riid)
-        {
-            AddRef();
-            *ppv = (IAudioSessionNotification*)this;
-        }
-        else
-        {
-            *ppv = NULL;
-            return E_NOINTERFACE;
-        }
-        return S_OK;
-    }
-    ULONG STDMETHODCALLTYPE AddRef(){
-        return InterlockedIncrement(&m_cRefAll);
-    }
-     
-    ULONG STDMETHODCALLTYPE Release(){
-        ULONG ulRef = InterlockedDecrement(&m_cRefAll);
-        if (0 == ulRef)
-        {
-			std::cout << "\n\nI am delete godbye\n";
-            delete this;
-        }
-        return ulRef;
-    }
-
-    HRESULT OnSessionCreated(IAudioSessionControl *pNewSession){
-        std::cout << "\n\n I AM IN THE OnSessionCreated\n";
-        HRESULT hr = S_OK;
-
-        ISimpleAudioVolume* pSimpleAudio = NULL;
-
-        hr = pNewSession->QueryInterface(__uuidof(ISimpleAudioVolume), (void**)&pSimpleAudio);
-        PRINT_ON_ERROR(hr);
-
-        wchar_t *dispName;
-        pNewSession->GetDisplayName(&dispName);
-		std::cout << "I am: " << pNewSession << "\n";
-        std::wcout << "Name: " << dispName;
-        float vol = -1;
-        pSimpleAudio->GetMasterVolume(&vol);
-        std::cout << "\nVolume: " << vol << "\n\n";
+	bool updated = false;
+	std::vector<ProgramVolume*> audioClients;
 
 
-        //SAFE_RELEASE(pNewSession);
-        SAFE_RELEASE(pSimpleAudio);
+	void add(ProgramVolume *p) {
+		audioClients.push_back(p);
+		newClients.push_back(audioClients.size() - 1);
+		updated = true;
+	}
 
-        return S_OK;
-    }
+	/*void add(IAudioSessionControl* pNewSession) {
+		ProgramVolume pv(pNewSession);
+		audioClients.push_back(pv);
+		newClients.push_back(audioClients.size() - 1);
+	}*/
+
+	void registerNewClients() {
+		for (size_t i : newClients) {
+			audioClients.at(i)->registerClient();
+		}
+
+		newClients.clear();
+	}
+
+	void cleanupList() {
+		std::vector<ProgramVolume*> toRemove;
+		for (ProgramVolume *p : this->audioClients) {
+			bool ret = p->release();
+			if (ret) {
+				updated = true;
+				toRemove.push_back(p);
+			}
+		}
+
+		std::erase_if(audioClients, [&toRemove](ProgramVolume* p) {
+		for (auto removeP : toRemove)
+		{
+			if (p == removeP) return true;
+		}
+		return false; });
+	}
 };
 
-//-----------------------------------------------------------
-// Client implementation of IAudioSessionEvents interface.
-// WASAPI calls these methods to notify the application when
-// a parameter or property of the audio session changes.
-//-----------------------------------------------------------
-class CAudioSessionEvents : public IAudioSessionEvents
-{
-private:
-	LONG _cRef;
+ProgVolumeList audioClients;
 
-	~CAudioSessionEvents()
-	{
-	}
+class AudioNotifications : public IAudioSessionNotification
+{
+
+private:
+
+	LONG             m_cRefAll;
+	~AudioNotifications() {};
 
 public:
-	CAudioSessionEvents() :
-		_cRef(1)
+	AudioNotifications() : m_cRefAll(1) {};
+
+	HRESULT STDMETHODCALLTYPE QueryInterface(REFIID riid, void** ppv)
 	{
+		if (IID_IUnknown == riid)
+		{
+			AddRef();
+			*ppv = (IUnknown*)this;
+		}
+		else if (__uuidof(IAudioSessionNotification) == riid)
+		{
+			AddRef();
+			*ppv = (IAudioSessionNotification*)this;
+		}
+		else
+		{
+			*ppv = NULL;
+			return E_NOINTERFACE;
+		}
+		return S_OK;
+	}
+	ULONG STDMETHODCALLTYPE AddRef() {
+		return InterlockedIncrement(&m_cRefAll);
 	}
 
-	// IUnknown methods -- AddRef, Release, and QueryInterface
-
-	ULONG STDMETHODCALLTYPE AddRef()
-	{
-		return InterlockedIncrement(&_cRef);
-	}
-
-	ULONG STDMETHODCALLTYPE Release()
-	{
-		ULONG ulRef = InterlockedDecrement(&_cRef);
+	ULONG STDMETHODCALLTYPE Release() {
+		ULONG ulRef = InterlockedDecrement(&m_cRefAll);
 		if (0 == ulRef)
 		{
+			std::cout << "\n\nI am delete godbye\n";
 			delete this;
 		}
 		return ulRef;
 	}
 
-	HRESULT STDMETHODCALLTYPE QueryInterface(
-		REFIID  riid,
-		VOID** ppvInterface)
-	{
-		if (IID_IUnknown == riid)
-		{
-			AddRef();
-			*ppvInterface = (IUnknown*)this;
-		}
-		else if (__uuidof(IAudioSessionEvents) == riid)
-		{
-			AddRef();
-			*ppvInterface = (IAudioSessionEvents*)this;
-		}
-		else
-		{
-			*ppvInterface = NULL;
-			return E_NOINTERFACE;
-		}
-		return S_OK;
-	}
+	HRESULT OnSessionCreated(IAudioSessionControl* pNewSession) {
+		std::cout << "\n\n I AM IN THE OnSessionCreated\n";
+		HRESULT hr = S_OK;
 
-	// Notification methods for audio session events
+		ISimpleAudioVolume* pSimpleAudio = NULL;
 
-	HRESULT STDMETHODCALLTYPE OnDisplayNameChanged(
-		LPCWSTR NewDisplayName,
-		LPCGUID EventContext)
-	{
-		return S_OK;
-	}
+		hr = pNewSession->QueryInterface(__uuidof(ISimpleAudioVolume), (void**)&pSimpleAudio);
+		PRINT_ON_ERROR(hr);
 
-	HRESULT STDMETHODCALLTYPE OnIconPathChanged(
-		LPCWSTR NewIconPath,
-		LPCGUID EventContext)
-	{
-		return S_OK;
-	}
+		wchar_t* dispName;
+		pNewSession->GetDisplayName(&dispName);
+		std::cout << "I am: " << pNewSession << "\n";
+		std::wcout << "Name: " << dispName;
+		float vol = -1;
+		pSimpleAudio->GetMasterVolume(&vol);
+		std::cout << "\nVolume: " << vol << "\n\n";
 
-	HRESULT STDMETHODCALLTYPE OnSimpleVolumeChanged(
-		float NewVolume,
-		BOOL NewMute,
-		LPCGUID EventContext)
-	{
-		if (NewMute)
-		{
-			printf("MUTE\n");
-		}
-		else
-		{
-			printf("Volume = %d percent\n",
-				(UINT32)(100 * NewVolume + 0.5));
-		}
 
-		return S_OK;
-	}
+		SAFE_RELEASE(pSimpleAudio);
 
-	HRESULT STDMETHODCALLTYPE OnChannelVolumeChanged(
-		DWORD ChannelCount,
-		float NewChannelVolumeArray[],
-		DWORD ChangedChannel,
-		LPCGUID EventContext)
-	{
-		return S_OK;
-	}
-
-	HRESULT STDMETHODCALLTYPE OnGroupingParamChanged(
-		LPCGUID NewGroupingParam,
-		LPCGUID EventContext)
-	{
-		return S_OK;
-	}
-
-	HRESULT STDMETHODCALLTYPE OnStateChanged(
-		AudioSessionState NewState)
-	{
-        std::string pszState = "?????";
-
-		switch (NewState)
-		{
-		case AudioSessionStateActive:
-			pszState = "active";
-			break;
-		case AudioSessionStateInactive:
-			pszState = "inactive";
-			break;
-		}
-		printf("New session state = %s\n", pszState.c_str());
-
-		return S_OK;
-	}
-
-	HRESULT STDMETHODCALLTYPE OnSessionDisconnected(
-		AudioSessionDisconnectReason DisconnectReason)
-	{
-        std::string pszReason = "?????";
-
-		switch (DisconnectReason)
-		{
-		case DisconnectReasonDeviceRemoval:
-			pszReason = "device removed";
-			break;
-		case DisconnectReasonServerShutdown:
-			pszReason = "server shut down";
-			break;
-		case DisconnectReasonFormatChanged:
-			pszReason = "format changed";
-			break;
-		case DisconnectReasonSessionLogoff:
-			pszReason = "user logged off";
-			break;
-		case DisconnectReasonSessionDisconnected:
-			pszReason = "session disconnected";
-			break;
-		case DisconnectReasonExclusiveModeOverride:
-			pszReason = "exclusive-mode override";
-			break;
-		}
-		printf("Audio session disconnected (reason: %s)\n",
-			pszReason.c_str());
+		mtx.lock();
+		ProgramVolume *pv = new ProgramVolume(pNewSession);
+		audioClients.add(pv);
+		mtx.unlock();
 
 		return S_OK;
 	}
 };
 
+
+
 int main()
 {
 	HRESULT hr = S_OK;
-/*
+
 	sp_port* ports;
 
 	check(sp_get_port_by_name("COM4", &ports));
@@ -308,8 +204,8 @@ int main()
 	sp_flush(ports, SP_BUF_BOTH);
 
     struct sp_port *rx_port = ports;
-    struct sp_port *tx_port;
-*/
+    struct sp_port *tx_port = ports;
+
     hr = CoInitializeEx(NULL, COINIT_MULTITHREADED);
 	PRINT_ON_ERROR(hr);
 
@@ -351,8 +247,6 @@ int main()
     
     AudioNotifications *notifying = new AudioNotifications();
 
-	//IAudioSessionNotification *pAudioSessionNotification = NULL;
-	//notifying->QueryInterface(__uuidof(IAudioSessionNotification), (void **) & pAudioSessionNotification);
 
     hr = pAudioSessionManager2->RegisterSessionNotification(notifying);
 	PRINT_ON_ERROR(hr);
@@ -365,28 +259,17 @@ int main()
     PRINT_ON_ERROR(hr)
 
     std::cout << "called the functions\n";
-    CAudioSessionEvents *audiosess = new CAudioSessionEvents();
-    pAudioSessionControl->RegisterAudioSessionNotification(audiosess);
-
+	mtx.lock();
     for (int i = 0; i < tempcount; i++) {
         hr = pAudioSessionEnumerator->GetSession(i, &pAudioSessionControl);
         PRINT_ON_ERROR(hr);
 
-        hr = pAudioSessionControl->QueryInterface(__uuidof(ISimpleAudioVolume), (void**)&pSimpleAudio);
-        PRINT_ON_ERROR(hr);
+		ProgramVolume *pv = new ProgramVolume(pAudioSessionControl);
 
-        wchar_t *dispName;
-        pAudioSessionControl->GetDisplayName(&dispName);
-        std::wcout << "Name: " << dispName;
-        float vol = -1;
-        pSimpleAudio->GetMasterVolume(&vol);
-        std::cout << "\n Volume: " << vol << "\n\n";
-
-		
-		SAFE_RELEASE(pSimpleAudio);
-        SAFE_RELEASE(pAudioSessionControl);
+		audioClients.add(pv);
 
     }
+	mtx.unlock();
 
     std::cout << "Enumerating through the things\n";
 
@@ -394,74 +277,103 @@ int main()
 	while (true)
 	{
 
-        /*
-		int result = check(sp_blocking_read(rx_port, buf, 6, timeout));
+		//Read from USB
+		int result = check(sp_nonblocking_read(rx_port, buf, 16));
 		buf[result] = '\0';
 		masterbuf.append(buf);
 		int rotCW = -1;
 		int index = -1;
 
-		while (true) {
-			int searched_index = masterbuf.find("\n");
-			if (searched_index != -1) {
-				std::string usbmessage = masterbuf.substr(0, searched_index);
-				masterbuf = masterbuf.erase(0, searched_index+1);
+		int searched_index = masterbuf.find("\n");
+		if (searched_index != -1) {
+			std::string usbmessage = masterbuf.substr(0, searched_index);
+			masterbuf = masterbuf.erase(0, searched_index + 1);
 
-				std::cout << usbmessage << "\n";
+			std::cout << usbmessage << "\n";
 
-				int comsplit = usbmessage.find(",");
-				if (comsplit != -1) {
-					index = stoi(usbmessage.substr(0, comsplit));
-					rotCW = stoi(usbmessage.substr(comsplit + 1, std::string::npos));
-				}
+			int comsplit = usbmessage.find(",");
+			if (comsplit != -1) {
+				index = stoi(usbmessage.substr(0, comsplit));
+				rotCW = stoi(usbmessage.substr(comsplit + 1, std::string::npos));
+			}
 
-				if (index < 0 || rotCW < 0) {
-					std::cout << "SOMETHING IS WRONG INDEX: " << index << " rotCW: " << rotCW;
-				}
-
+			if (index < 0 || rotCW < 0) {
+				std::cout << "SOMETHING IS WRONG INDEX: " << index << " rotCW: " << rotCW;
+			}
 
 
-				if (index == 0) {
+
+			if (index == 0) {
+				float vol = 0;
+				pAudioEndpointVolume->GetMasterVolumeLevelScalar(&vol);
+				if (rotCW)
+					vol = vol + 0.01 < 100 ? vol + 0.01 : vol;
+				else
+					vol = vol - 0.01 < 100 ? vol - 0.01 : vol;
+				pAudioEndpointVolume->SetMasterVolumeLevelScalar(vol, NULL);
+			}
+			else {
+				index--;
+
+				mtx.lock();
+				if (index < audioClients.audioClients.size()) {
 					float vol = 0;
-					pAudioEndpointVolume->GetMasterVolumeLevelScalar(&vol);
+					vol = audioClients.audioClients.at(index)->getVolume();
 					if (rotCW)
 						vol = vol + 0.01 < 100 ? vol + 0.01 : vol;
 					else
 						vol = vol - 0.01 < 100 ? vol - 0.01 : vol;
-					pAudioEndpointVolume->SetMasterVolumeLevelScalar(vol, NULL);
+					audioClients.audioClients.at(index)->setVolume(vol);
 				}
-				else {
-					index--;
-					
-					mtx.lock();
-					if (index < volumeVector.size()) {
-						float vol = 0;
-						vol = volumeVector.at(index).getVolume();
-						if (rotCW)
-							vol = vol + 0.01 < 100 ? vol + 0.01 : vol;
-						else
-							vol = vol - 0.01 < 100 ? vol - 0.01 : vol;
-						volumeVector.at(index).setVolume(vol);
-					}
-					mtx.unlock();
-				}
+				mtx.unlock();
+			}
 
 
-			}
-			else {
-				break;
-			}
 		}
-    */
+		
+		//Write To USB
+		mtx.lock();
+		if (audioClients.updated) {
+			char cprogname[50] = "";
+			size_t ksjdflaksdjflkdjsf = 20;
+			std::cout << "\n\nSending DATA \n\n";
+			char start[] = "s\n";
+			std::cout << "Start: " << check(sp_blocking_write(tx_port, start, std::strlen(start), 0)) << "\n\n";
+
+			for (size_t j = 0; j < audioClients.audioClients.size(); ++j) {
+				std::string data_send;
+				if (wcstombs_s(&ksjdflaksdjflkdjsf, cprogname, (size_t)50, audioClients.audioClients.at(j)->display_name.c_str(), 50) == EINVAL) {
+					std::cout << "ERROR CAN'T COPY\n";
+				}
+				//data_send.append("+,");
+				data_send.append(cprogname);
+				data_send.append("\n");
+				std::cout << "Size: " << data_send.size() << " " << data_send.size() << " Data send: \"" << data_send.c_str() << "\"\n";
+				std::cout << "Bytes: " << check(sp_blocking_write(tx_port, data_send.c_str(), std::strlen(data_send.c_str()), 0)) << "\n";
+			}
+
+			char end[] = "e\n";
+			std::cout << "End: " << check(sp_blocking_write(tx_port, end, std::strlen(end), 0)) << "\n\n";
+			
+			audioClients.updated = false;
+		}
+
+		//Register new clients
+		
+		audioClients.registerNewClients();
+		audioClients.cleanupList();
+
+		mtx.unlock();
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
 	}
 
 
 	//Clean up----------------------------------------------------------------------------
-//	free(buf);
+	free(buf);
 
-//	sp_free_port(ports);
+	sp_free_port(ports);
 
-    pAudioSessionControl->UnregisterAudioSessionNotification(audiosess);
 	pAudioSessionManager2->UnregisterSessionNotification(notifying);
 	SAFE_RELEASE(pAudioSessionEnumerator);
 	SAFE_RELEASE(pAudioEndPoint);
